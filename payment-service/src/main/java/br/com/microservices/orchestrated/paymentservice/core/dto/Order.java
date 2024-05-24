8507e528a3cf8aae420090fdc136cfbadcb246d2
Joao Pedro De Munno Durante <joao.durante@venturus.org.br>
2024-05-24 19:58:18 -0300

    DTOs created

---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Order.java
@@ -0,0 +1,20 @@
+package br.com.microservices.orchestrated.paymentservice.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private String id;
+    private List<OrderProduct> products;
+    private double totalAmount;
+    private int totalItems;
+    private LocalDateTime createdAt;
+    private String transactionId;
+}
