40a5ba5b4c9e232dc7ace2934b678b0e8ec9ccc7
Joao Pedro De Munno Durante <joao.durante@venturus.org.br>
2024-05-27 14:29:03 -0300

    implemented exceptions

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/config/exception/ExceptionGlobalHandler.java
@@ -0,0 +1,15 @@
+package br.com.microservices.orchestrated.productvalidationservice.config.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionGlobalHandler {
+    @ExceptionHandler
+    public ResponseEntity<?> handleValidationException(ValidationException exception) {
+        var details = new ExceptionDetails(HttpStatus.BAD_GATEWAY.value(), exception.getMessage());
+        return new ResponseEntity<>(details, HttpStatus.BAD_GATEWAY);
+    }
+}
