b87abbb03456df0f6dc83962644466ad83168fe1
Joao Pedro De Munno Durante <joao.durante@venturus.org.br>
2024-05-27 14:46:33 -0300

    kafka producer implemented

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/producer/KafkaProducer.java
@@ -0,0 +1,26 @@
+package br.com.microservices.orchestrated.inventoryservice.core.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class KafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${spring.kafka.topic.orchestrator}")
+    private String topic;
+
+    public void sendTemplate(String payload) {
+        try {
+            kafkaTemplate.send(topic, payload);
+            log.info("{} sent to topic {}", payload, topic);
+        } catch (Exception e) {
+            log.error("Failed to send {} to topic {}", payload, topic);
+        }
+    }
+}
---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/producer/KafkaProducer.java
@@ -0,0 +1,24 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.producer;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class KafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendTemplate(String topic, String payload) {
+        try {
+            kafkaTemplate.send(topic, payload);
+            log.info("{} sent to topic {}", payload, topic);
+        } catch (Exception e) {
+            log.error("Failed to send {} to topic {}", payload, topic);
+        }
+    }
+}
